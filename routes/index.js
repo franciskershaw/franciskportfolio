110eca061399fec37d0d96aae618904d5fd963b8
Francis Kershaw <franciskershaw@192.168.1.119>
2022-01-02 18:27:28 +0000

    render basic navbar

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const passport = require('passport');
 
 const Administrator = require('../models/Administrator')
 
