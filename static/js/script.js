34ad6e5a3883876853bf8e51c924701c279efc82
Francis Kershaw <franciskershaw@192.168.1.119>
2022-01-03 11:09:23 +0000

    finish nav animation

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,24 @@
-console.log('HI!');
+const navSlide = () => {
+    console.log('HELLO!');
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    // Toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .25}s`;
+            }
+        });
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    navSlide();
+})
