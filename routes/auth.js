04e4bda3c47a00b2288ff5803e6727fdf2e33f1d
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-01-28 15:33:15 +0000

    test commit 3

--- routes/auth.js
+++ routes/auth.js
@@ -5,19 +5,19 @@ const bcrypt = require('bcrypt');
 const Administrator = require('../models/Administrator')
 
 // @desc    Temporary register page for hashing passwords
-// @route   GET /register
+// @route   GET /auth/register
 router.get('/register', (req, res) => {
     res.render('register-temp')
 })
 
 // @desc    Render admin login page
-// @route   GET /admin
+// @route   GET /auth/admin
 router.get('/admin', (req, res) => {
     res.render('login')
 })
 
 // @desc    Handle registration
-// @route   POST /register
+// @route   POST /auth/register
 router.post('/register', async (req, res) => {
     
 })
