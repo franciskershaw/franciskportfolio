adb558399dff466d580f55e839a2d9a91e23e113
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-04-02 12:00:47 +0100

    redo navbar in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <>
       <div className="container">
-        <Navbar />
-        <HomePage />
+        <Header />
       </div>
     </>
   );
