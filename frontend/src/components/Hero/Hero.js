d61b233cc7c49726287fce4859e041cb366e578d
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-04-05 16:39:46 +0100

    clean up backend, start hero section

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -3,7 +3,12 @@ import classes from './Hero.module.scss';
 const Hero = () => {
 	return (
 		<div id='hero' className={classes.hero}>
-			I'm the hero/about section
+			<div className={classes.heroLeft}>
+				<div className={classes.imgContainer}>
+					<img src="assets/hero-image.jpeg" alt="" />
+				</div>
+			</div>
+			<div className={classes.heroRight}></div>
 		</div>
 	)
 }
