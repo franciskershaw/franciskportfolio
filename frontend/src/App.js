c51e1d48828c60b96291b822f18b761db45b7401
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-04-05 15:34:44 +0100

    style responsive nav and hamburger

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Contact from './components/Contact/Contact';
 import Projects from './components/Projects/Projects';
+import { useState } from 'react';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className='page-container'>
-      <Header />
+    <div className="page-container">
+      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="page-content">
         <Hero />
         <Projects />
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,19 +1,29 @@
 import classes from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-const Header = () => {
-
+const Header = (props) => {
   return (
-    <header className={classes.header}>
+    <header
+      className={`${classes.header} ${props.menuOpen && classes.headerActive}`}>
       <div className={classes.headerWrapper}>
         <div className={classes.headerWrapperLeft}>
-          <a className={classes.logo} href="#hero">francisKershaw.</a>
+          <a className={classes.logo} href="#hero">
+            {`<FrancisKershawDev />`}
+          </a>
           <div className={classes.iconContainer}>
-            <span>LI</span>
-            <span>GH</span>
+            <FontAwesomeIcon className={classes.socialIcon} icon={faLinkedin} />
+            <FontAwesomeIcon className={classes.socialIcon} icon={faGithub} />
           </div>
         </div>
         <div className={classes.headerWrapperRight}>
-          Right side
+          <div
+            onClick={() => props.setMenuOpen(!props.menuOpen)}
+            className={classes.hamburger}>
+            <span className={classes.line1}></span>
+            <span className={classes.line2}></span>
+            <span className={classes.line3}></span>
+          </div>
         </div>
       </div>
     </header>
