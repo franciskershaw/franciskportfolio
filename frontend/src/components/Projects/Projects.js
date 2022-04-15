09c0881595d33a793e918250585bd05ada58e8a5
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-04-15 21:59:59 +0100

    Hard code project swiper with pagination

--- frontend/src/components/Projects/Projects.js
+++ frontend/src/components/Projects/Projects.js
@@ -1,25 +1,36 @@
 import { useEffect, useState } from 'react';
-// import { fetchProjects } from '../../requests/requests';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper';
 
 const Projects = () => {
-  const [projects, setProjects] = useState([{}])
-  
+  const [projects, setProjects] = useState([{}]);
+
   useEffect(() => {
-    fetch('/api/projects').then(
-      response => response.json()
-    ).then (
-      data => {
-        setProjects(data)
-      }
-    )
+    fetch('/api/projects')
+      .then((response) => response.json())
+      .then((data) => {
+        setProjects(data);
+      });
   }, []);
 
   return (
     <div id="projects" className="projects">
-      <h2>My recent work</h2>
-      {projects.map((project, index) => (
+      {/* <h2>My recent work</h2> */}
+      {/* {projects.map((project, index) => (
         <div key={`project_${index}`}>{project.title}</div>
-      ))}
+      ))} */}
+      <div className="swiper">
+        <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+          <SwiperSlide>Project 1</SwiperSlide>
+          <SwiperSlide>Project 2</SwiperSlide>
+          <SwiperSlide>Project 3</SwiperSlide>
+          <SwiperSlide>Project 4 4</SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
