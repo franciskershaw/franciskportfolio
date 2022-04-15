98b903c4b567f6872571c3c57650bebae0d79fc1
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-04-16 00:24:32 +0100

    position elements correctly within card component

--- frontend/src/components/Projects/ProjectCard/ProjectCard.js
+++ frontend/src/components/Projects/ProjectCard/ProjectCard.js
@@ -5,12 +5,19 @@ const ProjectCard = (props) => {
   return (
     <div className="projects__card">
       <div className="projects__card--container">
-        <h2>{props.project.title}</h2>
-        <div className="projects_card--image">
+        <h3>{props.project.title}</h3>
+        <div className="projects__card--skills">
+          skills go here
+        </div>
+        <div className="projects__card--image">
           <picture>
             <source srcSet={placeholder} />
             <img srcSet={placeholder} />
           </picture>
+        </div>
+        <div className="projects_card--buttons">
+          <a className='btn btn--small' href="#">Git Repo</a>
+          <a className='btn btn--small' href="#">Live Site</a>
         </div>
       </div>
     </div>
