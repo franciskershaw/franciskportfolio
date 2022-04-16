333da2ca18bfdab26bc499afcb4053491ac7f4cf
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-04-16 11:57:35 +0100

    Add product card images and render them in component

--- frontend/src/components/Projects/ProjectCard/ProjectCard.js
+++ frontend/src/components/Projects/ProjectCard/ProjectCard.js
@@ -1,23 +1,27 @@
-import placeholder from '../../../assets/amiresponsive.png';
+import { useEffect, useState } from 'react';
 
-const ProjectCard = (props) => {
+
+const ProjectCard = (props) => { 
 
   return (
     <div className="projects__card">
       <div className="projects__card--container">
         <h3>{props.project.title}</h3>
-        <div className="projects__card--skills">
-          skills go here
-        </div>
-        <div className="projects__card--image">
-          <picture>
-            <source srcSet={placeholder} />
-            <img srcSet={placeholder} />
-          </picture>
-        </div>
+        <div className="projects__card--skills">placeholder</div>
+        {props.project.screenShotOne && (
+          <div className="projects__card--image">
+            <picture>
+              <img src={require(`../../../assets/${props.project.screenShotOne}_multidevice.png`)} alt="Project on multiple devices" />
+            </picture>
+          </div>
+        )}
         <div className="projects_card--buttons">
-          <a className='btn btn--small' href="#">Git Repo</a>
-          <a className='btn btn--small' href="#">Live Site</a>
+          <a className="btn btn--small" href="#">
+            Git Repo
+          </a>
+          <a className="btn btn--small" href="#">
+            Live Site
+          </a>
         </div>
       </div>
     </div>
