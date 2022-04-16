151d296497a05cb9c33e8bfa34f0886907dd99dc
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-04-16 12:02:54 +0100

    add repo and live site links to card component

--- frontend/src/components/Projects/ProjectCard/ProjectCard.js
+++ frontend/src/components/Projects/ProjectCard/ProjectCard.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 
-
-const ProjectCard = (props) => { 
-
+const ProjectCard = (props) => {
   return (
     <div className="projects__card">
       <div className="projects__card--container">
@@ -11,15 +9,27 @@ const ProjectCard = (props) => {
         {props.project.screenShotOne && (
           <div className="projects__card--image">
             <picture>
-              <img src={require(`../../../assets/${props.project.screenShotOne}_multidevice.png`)} alt="Project on multiple devices" />
+              <source
+                srcSet={require(`../../../assets/${props.project.screenShotOne}_multidevice.png`)}
+              />
+              <img
+                src={require(`../../../assets/${props.project.screenShotOne}_multidevice.png`)}
+                alt="Project on multiple devices"
+              />
             </picture>
           </div>
         )}
         <div className="projects_card--buttons">
-          <a className="btn btn--small" href="#">
+          <a
+            className="btn btn--small"
+            href={props.project.gitRepo}
+            target="_blank">
             Git Repo
           </a>
-          <a className="btn btn--small" href="#">
+          <a
+            className="btn btn--small"
+            href={props.project.liveSite}
+            target="_blank">
             Live Site
           </a>
         </div>
