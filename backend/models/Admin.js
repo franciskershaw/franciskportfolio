d03eda7e7bab7ae8ff997f7cb7e1e450d03f0bca
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-04-18 13:18:20 +0100

    Amend admin schema to be user schema

--- backend/models/Admin.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const passportLocalMongoose = require('passport-local-mongoose');
-
-const AdminSchema = new mongoose.Schema({});
-AdminSchema.plugin(passportLocalMongoose);
-
-module.exports = mongoose.model('Admin', AdminSchema)
---
+++ backend/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: [true, 'Please add a username'],
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+  },
+  password: {
+    type: string,
+    required: [true, 'Please add a password'],
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
