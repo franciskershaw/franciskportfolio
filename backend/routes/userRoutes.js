28d6ac944b30bb57da039e0d859c7160f84736ab
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-04-18 19:24:08 +0100

    Add login functionality to userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,8 +4,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
 
-const User = require('../models/User')
+const User = require('../models/User');
 
+// Register user
 router.post('/', asyncHandler(async (req, res) => {
     const {userName, email, password} = req.body;
     
@@ -47,9 +48,30 @@ router.post('/', asyncHandler(async (req, res) => {
     }
 }))
 
-router.post('/login', (req, res) => {
-    res.status(200).json({message: 'This POST route will eventually be used to log admin users in'})
-})
+// Login a user
+router.post('/login', asyncHandler(async (req, res) => {
+    try {
+        const { userName, password } = req.body;
+
+        const user = await User.findOne({ userName })
+        
+        // Check credentials match
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.status(200).json({
+                _id: user._id,
+                userName: user.userName,
+                email: user.email,
+                token: generateToken(user._id),
+            })
+        } else {
+            res.status(401).json({message: 'Invalid credentials'})
+            throw new Error('Invalid credentials')
+        }
+    } catch (err) {
+        console.log(err)
+    }
+    
+}))
 
 // Generate token
 const generateToken = (id) => {
