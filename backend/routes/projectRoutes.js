0e63f252ad46ac14aeed584d44853dae94f12684
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-04-18 12:51:33 +0100

    Add basic post and put routes for projects

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -13,4 +13,36 @@ router.get('/', asyncHandler(async (req, res) => {
 	}
 }));
 
+router.post('/', asyncHandler(async (req, res) => {
+	try {
+		const project = await Project.create(req.body)
+		res.status(201).json(project)
+	} catch (err) {
+		console.log(err)
+	}
+}))
+
+router.put('/:id', asyncHandler(async (req, res) => {
+	try {
+		const project = await Project.findById(req.params.id)
+	
+		if (!project) {
+			res.status(404)
+			throw new Error('Project not found')
+		}
+
+		const updatedProject = await Project.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{ new: true}
+		)
+
+		res.status(200).json(updatedProject)
+	
+	} catch (err) {
+		console.log(err)
+	}
+	
+}))
+
 module.exports = router;
