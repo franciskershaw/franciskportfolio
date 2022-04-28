5a39ec6ad6ff4adb0f1f4b4993eda2c5c286c8b1
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-04-28 17:17:10 +0100

    commit all changes for pull on other computer

--- frontend/src/components/Projects/ProjectCard/ProjectCard.js
+++ frontend/src/components/Projects/ProjectCard/ProjectCard.js
@@ -1,11 +1,19 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 const ProjectCard = (props) => {
+  // const [skills, setSkills] = useState()
+
+  // console.log(props)
+
+  // const getSkills = (skills) => {
+  //   console.log(skills)
+  // }
+
   return (
     <div className="projects__card">
       <div className="projects__card--container">
         <h3>{props.project.title}</h3>
-        <div className="projects__card--skills">placeholder</div>
+        <div className="projects__card--skills">{props.project.subHeading}</div>
         {props.project.screenShotOne && (
           <div className="projects__card--image">
             <picture>
@@ -23,13 +31,15 @@ const ProjectCard = (props) => {
           <a
             className="btn btn--small"
             href={props.project.gitRepo}
-            target="_blank">
+            target="_blank"
+            rel="noreferrer">
             Git Repo
           </a>
           <a
             className="btn btn--small"
             href={props.project.liveSite}
-            target="_blank">
+            target="_blank"
+            rel="noreferrer">
             Live Site
           </a>
         </div>
