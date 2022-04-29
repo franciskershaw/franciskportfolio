b379c3bc05643b62c1ea34c5ea199bbaf42f13bf
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-04-29 15:40:30 +0100

    begin proper styling of project card comnponent

--- frontend/src/components/Projects/ProjectCard/ProjectCard.js
+++ frontend/src/components/Projects/ProjectCard/ProjectCard.js
@@ -1,33 +1,30 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ProjectCard = (props) => {
-  // const [skills, setSkills] = useState()
+  // const [skills, setSkills] = useState([])
 
-  // console.log(props)
+  // console.log(props.project.skills)
 
-  // const getSkills = (skills) => {
-  //   console.log(skills)
-  // }
+  // useEffect(() => {
+
+  // },[props.project.skills])
+
+  const style = {
+    borderTop: `thick solid ${props.project.backgroundColor}`
+  }
 
   return (
     <div className="projects__card">
-      <div className="projects__card--container">
-        <h3>{props.project.title}</h3>
-        <div className="projects__card--skills">{props.project.subHeading}</div>
-        {props.project.screenShotOne && (
-          <div className="projects__card--image">
-            <picture>
-              <source
-                srcSet={require(`../../../assets/${props.project.screenShotOne}_multidevice.png`)}
-              />
-              <img
-                src={require(`../../../assets/${props.project.screenShotOne}_multidevice.png`)}
-                alt="Project on multiple devices"
-              />
-            </picture>
-          </div>
-        )}
-        <div className="projects_card--buttons">
+      <div className="projects__card--container" style={style}>
+        <div className="projects__card--title">
+          <h3>{props.project.title}</h3>
+          <h4>{props.project.subHeading}</h4>
+        </div>
+        <div className="projects__card--skills">
+          <span>1 skill</span>
+          <span>2 skill</span>
+        </div>
+        <div className="projects__card--buttons">
           <a
             className="btn btn--small"
             href={props.project.gitRepo}
@@ -43,6 +40,20 @@ const ProjectCard = (props) => {
             Live Site
           </a>
         </div>
+        {props.project.screenShotOne && (
+          <div className="projects__card--image">
+            <picture>
+              <source
+                srcSet={require(`../../../assets/${props.project.screenShotOne}_multidevice.png`)}
+              />
+              <img
+                src={require(`../../../assets/${props.project.screenShotOne}_multidevice.png`)}
+                alt="Project on multiple devices"
+              />
+            </picture>
+          </div>
+        )}
+        
       </div>
     </div>
   );
