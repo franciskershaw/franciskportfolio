98182d2e1d9962638119b72f3671eea96701e973
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-05-02 12:22:48 +0100

    clear form values from inputs when email is sent

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -2,38 +2,73 @@ import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-	const [message, setMessage] = useState(false)
+  const [thankYou, setThankYou] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-	const formRef = useRef()
-	
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		
-		emailjs.sendForm('service_2cbbr3y', 'template_zo0nvji', formRef.current, 'G3J6XdzvcbtYXlZIO')
-      .then((result) => {
+  const formRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_2cbbr3y',
+        'template_zo0nvji',
+        formRef.current,
+        'G3J6XdzvcbtYXlZIO'
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
+		
+		setUserName('')
+		setEmail('')
+		setMessage('')
+    setThankYou(true);
+  };
 
-		setMessage(true);
-	}
-
-	return (
-		<div id='contact' className='page contact'>
-			<h2 className='page__heading'>Get in touch!</h2>
-			<form ref={formRef} onSubmit={handleSubmit}>
-				<label htmlFor="user_name">Name</label>
-				<input type="text" id="user_name" name="user_name" />
-				<label htmlFor="user_email">Email address</label>
-				<input type="email" id="user_email" name="user_email" />
-				<label htmlFor="message">What's on your mind?</label>
-				<textarea name="message" id="message" cols="30" rows="10"></textarea>
-				<button type="submit">Send Message</button>
-				{message && <span>Thanks for your message, I'll get back to you ASAP :)</span>}
-			</form>
-		</div>
-	)
-}
+  return (
+    <div id="contact" className="page contact">
+      <h2 className="page__heading">Get in touch!</h2>
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <label htmlFor="user_name">Name</label>
+        <input
+          value={userName}
+          type="text"
+          id="user_name"
+          name="user_name"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <label htmlFor="user_email">Email address</label>
+        <input
+          value={email}
+          type="email"
+          id="user_email"
+          name="user_email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="message">What's on your mind?</label>
+        <textarea
+          value={message}
+          name="message"
+          id="message"
+          cols="30"
+          rows="10"
+          onChange={(e) => setMessage(e.target.value)}></textarea>
+        <button type="submit">Send Message</button>
+        {thankYou && (
+          <span>Thanks for your message, I'll get back to you ASAP :)</span>
+        )}
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
