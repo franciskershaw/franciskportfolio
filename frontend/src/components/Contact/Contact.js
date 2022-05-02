b580cc97475f6af7f8999feadb335bb546ac7e64
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-05-02 11:24:24 +0100

    render basic contact form with placeholder styles

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,7 +1,21 @@
 const Contact = () => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('Submitted!');
+	}
+
 	return (
 		<div id='contact' className='page contact'>
 			<h2 className='page__heading'>Get in touch!</h2>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="">Name</label>
+				<input type="text" />
+				<label htmlFor="">Email address</label>
+				<input type="email" />
+				<label htmlFor="">What's on your mind?</label>
+				<textarea name="" id="" cols="30" rows="10"></textarea>
+				<button type="submit">Send Message</button>
+			</form>
 		</div>
 	)
 }
