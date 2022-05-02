e8c5d012d2fe4979683896eac1b354fd463ff932
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-05-02 14:38:50 +0100

    Add basic styles to contact form

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -27,42 +27,56 @@ const Contact = () => {
           console.log(error.text);
         }
       );
-		
-		setUserName('')
-		setEmail('')
-		setMessage('')
+
+    setUserName('');
+    setEmail('');
+    setMessage('');
     setThankYou(true);
   };
 
   return (
     <div id="contact" className="page contact">
       <h2 className="page__heading">Get in touch!</h2>
-      <form ref={formRef} onSubmit={handleSubmit}>
-        <label htmlFor="user_name">Name</label>
-        <input
-          value={userName}
-          type="text"
-          id="user_name"
-          name="user_name"
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <label htmlFor="user_email">Email address</label>
-        <input
-          value={email}
-          type="email"
-          id="user_email"
-          name="user_email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="message">What's on your mind?</label>
-        <textarea
-          value={message}
-          name="message"
-          id="message"
-          cols="30"
-          rows="10"
-          onChange={(e) => setMessage(e.target.value)}></textarea>
-        <button type="submit">Send Message</button>
+      <form className='form' ref={formRef} onSubmit={handleSubmit}>
+        <div className="form__group">
+          <input
+            value={userName}
+            type="text"
+            id="user_name"
+            name="user_name"
+            onChange={(e) => setUserName(e.target.value)}
+            className="form__input"
+						placeholder="Name"
+						required
+          />
+        </div>
+        <div className="form__group">
+          <input
+            value={email}
+            type="email"
+            id="user_email"
+            name="user_email"
+            onChange={(e) => setEmail(e.target.value)}
+            className="form__input"
+						placeholder='Email address'
+						required
+          />
+        </div>
+        <div className="form__group">
+          <textarea
+            value={message}
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            onChange={(e) => setMessage(e.target.value)}
+						placeholder="What's on your mind?"
+            className="form__input"
+						required></textarea>
+        </div>
+        <div className="form__group form__group--buttons">
+          <button className='btn btn--small btn--primary' type="submit">Send Message</button>
+        </div>
         {thankYou && (
           <span>Thanks for your message, I'll get back to you ASAP :)</span>
         )}
