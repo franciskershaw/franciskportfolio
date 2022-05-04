625cd7b79917dc3d8d1cb90fce3aeb3e117bf983
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-05-04 21:29:16 +0100

    Fix breakpoints for swiper cards in Projects.js

--- frontend/src/components/Projects/Projects.js
+++ frontend/src/components/Projects/Projects.js
@@ -1,13 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper';
 import ProjectCard from './ProjectCard/ProjectCard';
+import AppContext from '../../context/AppContext';
 
 const Projects = () => {
   const [projects, setProjects] = useState([{}]);
+  const [slidesPerView, setSlidesPerView] = useState(2)
 
+  const { windowDimensions } = useContext(AppContext);
+  
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
@@ -19,6 +23,22 @@ const Projects = () => {
       });
   }, []);
 
+  useEffect(() => {
+    calcSlidesPerView()
+  }, [windowDimensions])
+
+  const calcSlidesPerView = () => {
+    if (windowDimensions.winWidth <= 600) {
+      setSlidesPerView(1)
+    } else if (windowDimensions.winWidth < 900) {
+      setSlidesPerView(2)
+    }  else if (windowDimensions.winWidth < 1200) {
+      setSlidesPerView(3)
+    } else  {
+      setSlidesPerView(4)
+    }
+  }
+
   return (
     <div id="projects" className="page projects">
       <div className='projects__title'>
@@ -29,9 +49,7 @@ const Projects = () => {
           pagination={true}
           modules={[Pagination]}
           className="mySwiper"
-          breakpoints={{
-            768: { width: 768, slidesPerView: 2 },
-          }}>
+          slidesPerView={slidesPerView}>
           {projects.map((project, index) => (
             <SwiperSlide key={`project-${index}`}>
               <ProjectCard project={project} />
