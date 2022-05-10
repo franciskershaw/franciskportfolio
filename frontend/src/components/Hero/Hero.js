e2220c12bb7ff5a17060e5ac1af3da44a378ee09
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-05-10 16:42:32 +0100

    style width and height border for hero section

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -3,37 +3,32 @@ import AppContext from '../../context/AppContext';
 import { init } from 'ityped';
 
 const Hero = () => {
-  const textRef = useRef();
+  const h1Ref = useRef();
 
   const { windowDimensions } = useContext(AppContext);
 
   useEffect(() => {
-    init(textRef.current, {
+    init(h1Ref.current, {
       showCursor: true,
       loop: false,
       disableBackTyping: true,
       strings: [`Hi, my name is Francis.`],
-      typeSpeed: 50
+      typeSpeed: 50,
     });
   }, []);
 
-  const nextText = () => {
-    console.log('Next!');
-  };
-
   return (
     <div id="hero" className="hero">
+      <div className="topSpanContainer">
+        <aside>Width: {windowDimensions.winWidth}px</aside>
+      </div>
       <div className="hero__heading">
         <h1 className="mb-medium">
-          <span ref={textRef}></span>
+          <span ref={h1Ref}></span>
         </h1>
         <h3>
           I'm a frontend developer, specialising in React but with experience
-          all over the stack.{' '}
-          <span style={{ fontSize: '1rem' }}>
-            - width: {windowDimensions.winWidth}px, height:{' '}
-            {windowDimensions.winHeight}px
-          </span>
+          all over the stack.
         </h3>
       </div>
       <div className="hero__cta">
@@ -42,6 +37,10 @@ const Hero = () => {
         </a>
         {/* <a href='#contact' className='btn btn--secondary'>Get in touch</a> */}
       </div>
+      <div className="bottomSpanContainer">
+        <aside>Height: {windowDimensions.winHeight}px</aside>
+      </div>
+      <div className="heightLine"></div>
     </div>
   );
 };
