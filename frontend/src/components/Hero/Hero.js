d89089e0df03c195f7165d5d63b8c44d3f5f7c84
Francis Kershaw <franciskershaw.dev@gmail.com>
2022-05-10 15:42:34 +0100

    add background image, overlay, and typing animation to hero section

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -1,23 +1,47 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import AppContext from '../../context/AppContext';
+import { init } from 'ityped';
 
 const Hero = () => {
+  const textRef = useRef();
+
   const { windowDimensions } = useContext(AppContext);
 
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      loop: false,
+      disableBackTyping: true,
+      strings: [`Hi, my name is Francis.`],
+      typeSpeed: 50
+    });
+  }, []);
+
+  const nextText = () => {
+    console.log('Next!');
+  };
+
   return (
-    <div id="hero" className='hero'>
-      <div className='hero__heading'>
-        <h1 className='mb-medium'>Hi there, I'm Francis.</h1>
-        <p>
+    <div id="hero" className="hero">
+      <div className="hero__heading">
+        <h1 className="mb-medium">
+          <span ref={textRef}></span>
+        </h1>
+        <h3>
           I'm a frontend developer, specialising in React but with experience
-          all over the stack. <span style={{'fontSize': '1rem'}}>- width: {windowDimensions.winWidth}px, height: {windowDimensions.winHeight}px</span>
-        </p>
+          all over the stack.{' '}
+          <span style={{ fontSize: '1rem' }}>
+            - width: {windowDimensions.winWidth}px, height:{' '}
+            {windowDimensions.winHeight}px
+          </span>
+        </h3>
       </div>
-			<div className='hero__cta'>
-				{/* Button components here */}
-				<a href='#projects' className='btn'>My work</a>
-        <a href='#contact' className='btn'>Get in touch</a>
-			</div>
+      <div className="hero__cta">
+        <a href="#projects" className="btn btn--secondary">
+          Find out more
+        </a>
+        {/* <a href='#contact' className='btn btn--secondary'>Get in touch</a> */}
+      </div>
     </div>
   );
 };
