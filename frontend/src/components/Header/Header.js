2757b8a37bf56c5bb9fd1b487c0e8159ab492330
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-06-02 15:01:52 +0200

    add nav links for large screen sizes

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -3,25 +3,39 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Header = (props) => {
   return (
-    <header
-      className={`header ${props.menuOpen && 'header--active'}`}>
-      <div className='header__wrapper'>
-        <div className='header__wrapper--left'>
-          <a className='logo' href="#hero">
+    <header className={`header ${props.menuOpen && 'header--active'}`}>
+      <div className="header__wrapper">
+        <div className="header__wrapper--left">
+          <a className="logo" href="#hero">
             {`<FrancisKershaw />`}
           </a>
-          <div className='iconContainer'>
-            <FontAwesomeIcon className='socialIcon' icon={faLinkedin} />
-            <FontAwesomeIcon className='socialIcon' icon={faGithub} />
+          <div className="iconContainer">
+            <a href="https://www.linkedin.com/in/francis-kershaw/">
+              <FontAwesomeIcon className="socialIcon" icon={faLinkedin} />
+            </a>
+            <a href="https://github.com/franciskershaw">
+              <FontAwesomeIcon className="socialIcon" icon={faGithub} />
+            </a>
           </div>
         </div>
-        <div className='header__wrapper--right'>
+        <ul>
+          <li>
+            <a href="#hero">Home</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <div className="header__wrapper--right">
           <div
             onClick={() => props.setMenuOpen(!props.menuOpen)}
-            className='hamburger'>
-            <span className='line1'></span>
-            <span className='line2'></span>
-            <span className='line3'></span>
+            className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
           </div>
         </div>
       </div>
