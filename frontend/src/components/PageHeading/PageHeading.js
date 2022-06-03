097b72f3619b690226d4696834f314ffb071439a
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-06-03 15:37:44 +0200

    add placeholder heading underline

--- frontend/src/components/PageHeading/PageHeading.js
+++ frontend/src/components/PageHeading/PageHeading.js
@@ -1,15 +1,22 @@
 import { useInView } from 'react-intersection-observer';
 import { useEffect } from 'react';
+import {motion}  from 'framer-motion';
 
 const PageHeading = (props) => {
   const { ref, inView } = useInView();
 
 	useEffect(() => {
 		console.log(`useEffect, ${props.section} inView = ${inView}`)
-	}, [inView]);
+	}, [inView, props.section]);
 
   return (
-		<h2 ref={ref} className="page__heading">{props.text}</h2>
+		<>
+			<motion.h2 ref={ref} className="page__heading">
+				{props.text}
+				<span className="page__heading--bottomBorder"></span>
+			</motion.h2>
+			
+		</>
 	);
 };
 
