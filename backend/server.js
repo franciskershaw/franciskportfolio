479b93f6a0412de2461ec8974cd7d0dc97a70b8d
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-06-04 13:20:16 +0200

    begin configuring cloudinary for eventual deployment

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,10 @@ app.use('/api/skills', require('./routes/skillRoutes'));
 
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to production' });
-  });
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
 } else {
   app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the FK Portfolio API' });
